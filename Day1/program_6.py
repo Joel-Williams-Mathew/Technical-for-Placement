10278a2d666ec963dca0757b18d2f7923973ff11
Joel-Williams-Mathew <joelw2200@gmail.com>
2026-06-22 14:32:04 +0530

    Program_6

---
+++ Day1/program_6.py
@@ -0,0 +1,28 @@
+'''
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4                         # when n = 5 ((ixi)-1)
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+'''
+
+n = int(input("Enter n: "))
+
+size = 2 * n - 1
+
+for i in range(size):        #row
+    for j in range(size):    #column
+
+        top = i
+        left = j
+        bottom = size - 1 - i
+        right = size - 1 - j
+
+        distance = min(top, left, bottom, right)
+
+        print(n - distance, end=" ")
+
+    print()
+
