4d200f40a6b796dc7c21953a687708309a74647b
Joel-Williams-Mathew <joelw2200@gmail.com>
2026-06-22 10:32:53 +0530

    progran 1 variations

--- Day1/program_1.py
+++ Day1/program_1.py
@@ -2,6 +2,12 @@ n = int(input("Enter a number: "))
 
 divisors = [2, 3, 5, 7, 11]
 
+# Variation 1
 for d in divisors:
     if n % d == 0:
         print(f"It is divisible by {d}")
+
+# Variation 2
+for i in range(1,12):
+    if n%i == 0:
+        print(f"It is divisible by {i}")
